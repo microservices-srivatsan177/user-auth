4cc74d2a9bd641f3c638457555b1ef86915a48b8
srivatsan177 <ksrivatsan177@gmail.com>
2024-07-13 18:44:29 +0530

    initial commit

---
+++ src/main/java/com/srivatsan177/e_commerce/user_auth/user/auth/UserAuthApplication.java
@@ -0,0 +1,13 @@
+package com.srivatsan177.e_commerce.user_auth.user.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthApplication.class, args);
+	}
+
+}
